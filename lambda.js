ec31832944affecd23a4a419beaafcda1fdb10b0
Chris Coffman <christopher.coffman@ocelotconsulting.com>
2016-10-03 10:17:02 -0500

    changing to all arrow functions

--- lambda.js
+++ lambda.js
@@ -15,16 +15,16 @@ function getS3(bucket, key){
 }
 
 function getJSON(url) {
-    var p = new Promise(function(resolve, reject){
-        https.get(url, function(response) {
+    var p = new Promise((resolve, reject) => {
+        https.get(url, (response) => {
             var body = '';
-            response.on('data', function(d) {
+            response.on('data', (d) => {
                 body += d;
             });
-            response.on('end', function() {
+            response.on('end', () => {
                 resolve(JSON.parse(body));
             });
-            response.on('error', function(err){
+            response.on('error', (err) => {
                 reject(err);
             });
         });
@@ -35,20 +35,20 @@ function getJSON(url) {
 exports.handler = (event, context, callback) => {
     var tokenInfoUrl = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' +
       event.Logins['accounts.google.com'];
-    getJSON(tokenInfoUrl).then(function(data){
+    getJSON(tokenInfoUrl).then((data) => {
       if(!data.email.endsWith('@ocelotconsulting.com')){
         throw new Error('Access denied');
       }
     })
-    .then(function(){
+    .then(() => {
       return getS3('ocelot-consulting-wp', `keys/pk-${keyPairId}.pem.txt`);
     })
-    .then(function(pk){
+    .then((pk) => {
       var options = {keypairId: `${keyPairId}`, privateKeyString: pk.Body.toString(),
       expireTime: moment().add(1, 'day')}
       callback(null, cf.getSignedCookies(`http*://${cloudFrontDomain}/*`, options));
     })
-    .catch(function(err){
+    .catch((err) => {
       console.log('error', err);
       callback(err);
     });
